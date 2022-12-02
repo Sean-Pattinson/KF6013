7fef46a7a89c76c367bac5457b4e62e1f6b9b99a
Sean-Pattinson <107890922+Sean-Pattinson@users.noreply.github.com>
2022-12-02 15:46:45 +0000

    Update config.php
    Remove sensitive info

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
-define('CONSUMER_KEY', 'NOKgCRhB26rsuvwZutUJxzPER');
-define('CONSUMER_SECRET', 'rn6I1vuylaR2l6PjQY5hwxAm6g19zTcbTYMbURzxRDqAA8rbEe');
-define('ACCESS_TOKEN', '2389088276-ed2LkrRb9mKj6yWpzoPv3a3JTGFY35ra7X7Zk68');
-define('ACCESS_SECRET', '3LNghVOTbT0aevIbn4DyNJKpjjzOeL4DPDlsH4RpsaRrg');
-define('OAUTH_CALLBACK', 'http:/20.26.215.5/KF6013/twitter_oauth.php');
-define('CLIENT_ID', 'VmhPVGxlUHRoYzc1Rk5ONm5ZMWc6MTpjaQ');
-define('CLIENT_SECRET', 'Zzsq7yyLsktiEi-XlwO669UjRkvmgHkO-6HxgVFP0PNoSrDTu6');
+define('CONSUMER_KEY', 'Your Consumer Key');
+define('CONSUMER_SECRET', 'Your Consumer Secret');
+define('ACCESS_TOKEN', 'Your Access Token');
+define('ACCESS_SECRET', 'Your Access Secret');
+define('OAUTH_CALLBACK', 'Your Callback');
+define('CLIENT_ID', 'Your CLient ID');
+define('CLIENT_SECRET', 'Your Client Secret');
 
 ?>
